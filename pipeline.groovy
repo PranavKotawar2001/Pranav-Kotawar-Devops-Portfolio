47766e21f8f87055ff37ef3ddd2ce1af06e1be3c
Pranav Kotawar <pranavkotawar@outlook.com>
2026-05-22 17:24:03 +0530

    pipeline is updated

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,36 +1,37 @@
 pipeline{
     agent any
     stages{
-        stage('Pull'){
+       stage('Pull'){
             steps{
-                git 'https://github.com/PranavKotawar2001/Pranav-Kotawar-Devops-Portfolio.git'
+                git branch: 'master',
+                url: 'https://github.com/PranavKotawar2001/Pranav-Kotawar-Devops-Portfolio.git'
             }
         }
 
-        stage('Test') {
-            steps {
-                withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'Sonar-secret') {
-                    sh '''
-                        sonar-scanner \
-                        -Dsonar.projectKey=python-app \
-                        -Dsonar.sources=. \
-                        -Dsonar.python.version=3
-                    '''
-                }
-            }
-        }
+        // stage('Test') {
+        //     steps {
+        //         withSonarQubeEnv(installationName: 'Sonar', credentialsId: 'Sonar-secret') {
+        //             sh '''
+        //                 sonar-scanner \
+        //                 -Dsonar.projectKey=python-app \
+        //                 -Dsonar.sources=. \
+        //                 -Dsonar.python.version=3
+        //             '''
+        //         }
+        //     }
+        // }
 
          stage('Docker-Image-Build'){
             steps{
-                sh '''docker build -t pranavsudhirkotawar/backend:latest .'''   
+                sh '''docker build -t pranavsudhirkotawar/portfolio:latest .'''   
             }
         }
-        
+
         stage('Push-Image-to-Docker-Hub'){
             steps{
                 sh '''
-                   docker push pranavsudhirkotawar/backend:latest
-                   docker rmi pranavsudhirkotawar/backend:latest''' 
+                   docker push pranavsudhirkotawar/portfolio:latest
+                   docker rmi pranavsudhirkotawar/portfolio:latest''' 
             }
         }
          
